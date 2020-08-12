7504baade2a11c8180e9cc996e09984cfc73e08d
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-08-12 11:39:11 +1000

    CSS added

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,8 @@ Property.destroy_all
 p1 = Property.create! name: 'Auburn square', address: '35 Northumberland Rd, Auburn, NSW', price: 600000, rooms: 4, bathrooms: 2, image: 'https://rimh2.domainstatic.com.au/HZr3aMrP9Mq7iTc3CuIrbAiUsvQ=/fit-in/1920x1080/filters:format(jpeg):quality(80):no_upscale()/http://b.domainstatic.com.au.s3-website-ap-southeast-2.amazonaws.com/908badf3-fa63-490a-b843-06f04d91d7a2-w1440-h1080'
 p2 = Property.create! name: 'River Realty', address: '36 Victoria Street, Mcmahons Point, NSW', price: 600000, rooms: 4, bathrooms: 2, image: 'https://rimh2.domainstatic.com.au/Yuovn_kxahV7G8OkPOw0Oab4QXM=/fit-in/1920x1080/filters:format(jpeg):quality(80):no_upscale()/http://b.domainstatic.com.au.s3-website-ap-southeast-2.amazonaws.com/2016405420_1_1_200728_073453-w4000-h2667'
 p3 = Property.create! name: 'Malvern Avenue', address: '49 Malvern Avenue, Manly NSW 2095', price: 600000, rooms: 4, bathrooms: 2, image: 'https://rimh2.domainstatic.com.au/hzVJfNiBmvhAjMwTSGvsewfxw6U=/fit-in/1920x1080/filters:format(jpeg):quality(80):no_upscale()/http://b.domainstatic.com.au.s3-website-ap-southeast-2.amazonaws.com/2016388660_1_1_200721_023310-w1600-h1200'
+p4 = Property.create! name: 'Robertson Park Avenue', address: '247 Lambert Road Indooroopilly QLD 4068
+', price: 2000000, rooms: 4, bathrooms: 2, image: 'https://rimh2.domainstatic.com.au/3XKhcljObekwUkQ-fCl6uIzMK_M=/fit-in/1920x1080/filters:format(jpeg):quality(80):no_upscale()/http://b.domainstatic.com.au.s3-website-ap-southeast-2.amazonaws.com/2016421009_1_1_200805_053452-w1600-h1200'
 
 puts "created #{Property.count} properties."
 
@@ -24,7 +26,7 @@ u3 = User.create! email: 'geet@ga.com', password: 'chicken', name: 'Geet'
 
 puts "Created #{User.count} users."
 
-u1.properties << p1 << p3
+u1.properties << p1 << p3 << p4
 u2.properties << p2
 
 print "User #{User.first.name} has properties:"
